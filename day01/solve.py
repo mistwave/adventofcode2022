ab88861b9d4e6c438056b39e9dd03267b7cb8332
Yifu Duan <duanyifu@hotmail.com>
2022-12-11 15:53:33 +0800

    improve reading input file

--- day01/solve.py
+++ day01/solve.py
@@ -1,15 +1,24 @@
+import sys
+from os import path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+from read_file import by_lines
+
+solve_dir = path.dirname(path.abspath(__file__))
+input_path = path.join(solve_dir, "input")
+content = by_lines(input_path)
 
 xs = []
 buf = 0
 
-with open("input", "r") as f: 
-    for line in f.readlines():
-        l = line.strip()
-        if l == "":
-            xs.append(buf)
-            buf = 0
-        else:
-            buf += int(l)
+
+for line in content:
+    l = line.strip()
+    if l == "":
+        xs.append(buf)
+        buf = 0
+    else:
+        buf += int(l)
 
 print("part 1:", max(xs))
 
---
+++ read_file.py
@@ -0,0 +1,11 @@
+from typing import List
+
+def by_lines(path: str) -> List[str]:
+    res = []
+    with open(path, 'r') as f:
+        for line in f.readlines():
+            res.append(line.strip())
+
+    return res
+
+
